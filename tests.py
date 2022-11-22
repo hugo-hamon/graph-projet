9f2744e7b2ea8e4650990e238b8b5c3f8720c562
Arkazix <hugohamon27@gmail.com>
2022-11-22 10:48:08 +0100

    update

--- tests.py
+++ tests.py
@@ -1,10 +1,4 @@
 from graph import Graph
-<<<<<<< HEAD
-g = Graph(9)
-shortest = g.brute_force()
-print(g.compute_distance_by_path(shortest))
-=======
 
 g = Graph(9)
 shortest = g.brute_force()
->>>>>>> master
