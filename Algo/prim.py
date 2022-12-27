77e2997c1839254b5904fb2b618fdc299036c984
Arkazix <hugohamon27@gmail.com>
2022-12-27 14:55:18 +0100

    Add heap prim

--- Algo/prim.py
+++ Algo/prim.py
@@ -2,6 +2,7 @@ from typing import List, Tuple
 from itertools import product
 from .graph import Graph
 import numpy as np
+import heapq
 
 
 class Prim(Graph):
@@ -10,7 +11,8 @@ class Prim(Graph):
         super().__init__(n, vertices)
 
     def compute(self) -> np.ndarray:
-        return self.__prim(0)
+        """Compute the minimum spanning tree of the graph"""
+        return self.__heap_prim(0)
 
     def __prim(self, s: int) -> np.ndarray:
         """Compute the minimum spanning tree of the graph"""
@@ -19,6 +21,22 @@ class Prim(Graph):
             possible_edges = self.__get_possible_edges(path)
             possible_edges.sort(key=lambda x: x[1])
             path.append(possible_edges[0][0][1])
+        return np.array(path)
+
+    def __heap_prim(self, s: int) -> np.ndarray:
+        """Compute the minimun spanning tree of the graph using a heap"""
+        path = [s]
+        heap = []
+        for i in range(self.n):
+            if i != s:
+                heapq.heappush(heap, (self.distances[s, i], s, i))
+        while len(path) < self.n:
+            _, s, i = heapq.heappop(heap)
+            if i not in path:
+                path.append(i)
+                for j in range(self.n):
+                    if j not in path:
+                        heapq.heappush(heap, (self.distances[i, j], i, j))
         return np.array(path)
 
     def __get_possible_edges(self, path: List[int]) -> List[Tuple[Tuple[int, int], int]]:
